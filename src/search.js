31de600242536ce5f610a983d76e6dbc2d9442a4
mnurfilza <choirfilza@gmail.com>
2019-12-16 00:24:26 +0700

    menambahkan api

--- src/search.js
+++ src/search.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 
 function Search() {
+  const [input, setInput] = useState({});
+ 
+ 
+  function searchHandle(e) { 
+    console.log(input)
+   }
+
+   function handleChange(e) {
+    setInput(e.target.value)
+   }
+  
   return (
     <div>
-        <input/>
+      <TextField  onChange={handleChange} id="filled-search" label="Search field"  margin="normal" type="search" variant="filled" />
+      <Button variant="contained" onClick={searchHandle} color="primary">
+        Search
+      </Button>
     </div>
   );
 }
+
 
 export default Search;
