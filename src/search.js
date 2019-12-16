d57e30e0da35dbdc0042163bcf281d34e1dc6807
mnurfilza <choirfilza@gmail.com>
2019-12-16 23:12:51 +0700

    pencarian menggunakan tittle sudah bisa

--- src/search.js
+++ src/search.js
@@ -1,23 +1,27 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 
 function Search() {
   const [input, setInput] = useState({});
- 
+  
  
   function searchHandle(e) { 
     console.log(input)
+    fetch(`http://www.omdbapi.com/?apikey=2dfe16a6&s=${input}`,{
+      method:"GET",
+    }).then(res=>{
+      return res.json()
+    }).then(response => {
+      console.log(response.Search)
+    })
    }
 
-   function handleChange(e) {
-    setInput(e.target.value)
-   }
-  
+   
   return (
     <div>
-      <TextField  onChange={handleChange} id="filled-search" label="Search field"  margin="normal" type="search" variant="filled" />
+      <TextField  onChange={e => setInput(e.target.value)} id="filled-search" label="Search field"  margin="normal" type="search" variant="filled" />
       <Button variant="contained" onClick={searchHandle} color="primary">
         Search
       </Button>
